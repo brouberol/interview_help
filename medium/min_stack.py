5a549307d928b2e3fe48a0f0d01b9b30ad77e5a2
Balthazar Rouberol <br@imap.cc>
2017-04-13 17:35:05 +0200

    Propose solution for min_stack.py

--- medium/min_stack.py
+++ medium/min_stack.py
@@ -4,3 +4,28 @@
 # Solution is to under the hood maintain 2 stacks, one is the actual stack, and
 # the other keeps the current minimum, and when the current minimum is popped
 # off the stack, pop it off the stack
+
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.mins = []
+
+    def __repr__(self):
+        return "<{} min={}>".format(self.__class__.__name__, self.min)
+
+    def push(self, value):
+        self.stack.append(value)
+        if not self.mins or value < self.mins[-1]:
+            self.mins.append(value)
+
+    def pop(self):
+        val = self.stack.pop()
+        if val == self.mins[-1]:
+            self.mins.pop()
+        return val
+
+    @property
+    def min(self):
+        if self.mins:
+            return self.mins[-1]
