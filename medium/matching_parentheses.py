a9701006393299f7cd35c3a11d32c9e5ed0c358e
Balthazar Rouberol <br@imap.cc>
2017-04-13 17:28:23 +0200

    Propose solution to matching_parentheses.py

--- medium/matching_parentheses.py
+++ medium/matching_parentheses.py
@@ -1,2 +1,17 @@
 # Write a function that given a string detects whether all parentheses are 
 # matched
+
+
+def balanced(s):
+    stack = []
+    for char in s:
+        if char not in ['(', ')']:
+            continue
+        elif char == '(':
+            stack.append(char)
+        elif char == ')':
+            if stack and stack[-1] == '(':
+                stack.pop()
+            else:
+                return False
+    return not stack
