9b565bd361cc362a2d5f8cdc3fe11d9722742d1b
Balthazar Rouberol <br@imap.cc>
2017-04-13 16:32:21 +0200

    Propose solution for bin_repr

--- medium/binary_representation.py
+++ medium/binary_representation.py
@@ -1,1 +1,16 @@
 # Given an integer, return as a string the binary representation of the integer.
+
+def bin_repr(i):
+    power, remainder = 0, i
+    while 2 ** power < i:
+        power += 1
+
+    out = ['0'] * (power + 1)
+
+    while remainder > 0:
+        if 2 ** power <= remainder:
+            remainder -= 2 ** power
+            out[power] = '1'
+        power -= 1
+
+    return ''.join(reversed(out)).lstrip('0')
