3d5bb63962110b72af63f782643d3ad81aa6b25b
Balthazar Rouberol <br@imap.cc>
2017-04-13 17:19:55 +0200

    Update solution to cypher_match
    The preexisting solution failed with `TypeError: cannot concatenate 'str' and 'int' objects`

--- medium/cipher_match.py
+++ medium/cipher_match.py
@@ -3,18 +3,15 @@
 # are not the same.  For example, “aabb” and “ccdd” are a cipher match, as are
 # “abccd” and “efggh”
 
-def cipher_match(str1, str2):
-  return string_to_cipher(str1) == string_to_cipher(str2)
+def cypher_equals(str1, str2):
+    return string_to_cypher(str1) == string_to_cypher(str2)
 
-def string_to_cipher(str):
-  used_chars = {}
-  cipher = ""
-  character_count = 0
-  for c in str:
-    dict_entry = used_chars.get(c)
-    if dict_entry:
-      cipher += dict_entry
-    else:
-      used_chars[c] = character_count
-      character_count += 1
-  return cipher
+
+def string_to_cypher(s):
+    cypher = []
+    previous = s[0]
+    for i, char in enumerate(s[1:]):
+        delta = ord(char) - ord(previous)
+        cypher.append(delta)
+        previous = char
+    return cypher
